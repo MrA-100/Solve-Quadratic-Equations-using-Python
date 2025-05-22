2c4227bce06b0c9a341cba6cbd216d329c24510a
MrA-100 <khurana.akshajt@gmail.com>
2025-05-23 01:54:02 +0530

    New features & Debugging
    1. Added a feature to make sure that the equation entered is quadratic (a isn't equal to 0)
    2. The plus and minus sign are shown properly when confirming the equation you entered.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Solve Quadratic Equations using Python
 A Python script where you can find the roots of Quadratic equation and even tell their nature (Real or Complex)
+<font size="1"> HELP: For roots of the equation which are not Rational numbers, the answer comes in decimals which are very long. Is there a way to show them in the fraction form itself. </font>
--- script.py
+++ script.py
@@ -1,10 +1,22 @@
 a = float(input("Enter the coefficient of x²:- "))
+while a==0:
+    a = float(input("The coefficient of x² can't be zero. Please retype:- "))
 b = float(input("Enter the coefficient of x:- "))
 c = float(input("Enter the coefficient of 1:- "))
-print ("Your quadratic equation is:")
-print (str(a) + "x² + " + str(b) + "x + " + str(c) + " = 0")
 
-dis = b*b - 4*a*c
+if b==abs(b):
+    bpositivity = "+"
+else:
+    bpositivity = "-"
+if c==abs(c):
+    cpositivity = "+"
+else:
+    cpositivity = "-"
+
+print ("Your quadratic equation is:")
+print (str(a) + "x² " + bpositivity + " " + str(b) + "x " + cpositivity + " " + str(c) + " = 0")
+
+dis = b**2 - 4*a*c
 print ("")
 if dis > 0:
     print ("The roots are two distinct real numbers.")
@@ -17,4 +29,7 @@ if dis >= 0:
     alpha = str((-1*b + dis**0.5) / (2*a))
     beta = str((-1*b - dis**0.5) / (2*a))
     print ("")
-    print ("The roots of the equation are " + alpha + " & " + beta)
+    if alpha!=beta:
+        print ("The roots of the equation are " + alpha + " & " + beta)
+    else:
+        print ("The root of the equation is " + alpha)
